f8cdae88ae30e29712667ec718b0ada1c2e558f0
Simran <simrangupta8770@gmail.com>
2023-09-26 21:49:38 +0530

    fir

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import AddExpense from "./pages/add-expense";
+import Receiptgen from "./pages/rgen/Receiptgen";
+import Home from "./pages/home/index";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/add-expense" element={<AddExpense />} />
+        <Route path="/receipt" element={<Receiptgen />} />
+      </Routes>
+      <Footer />
+      </>
   );
-}
+};
 
 export default App;
